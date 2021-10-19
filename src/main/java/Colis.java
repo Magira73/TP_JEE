2ccbccdcf9e0a36a0375adfcf36cd71cb9d91525
Magira73 <silvio_dompnier@hotmail.fr>
2021-10-19 14:05:48 +0200

    constr colis

--- src/main/java/Colis.java
+++ src/main/java/Colis.java
@@ -1,3 +1,4 @@
+
 public class Colis {
 	public static int ID = 0;
 	private final int id = ID++;
@@ -5,4 +6,11 @@ public class Colis {
 	private String val;
 	private Emplacement origine;
 	private Emplacement destination;
+
+	public Colis(String val, double poids, Emplacement src, Emplacement dest) {
+		this.poids = poids;
+		this.val = val;
+		this.origine = src;
+		this.destination = dest;
+	}
 }

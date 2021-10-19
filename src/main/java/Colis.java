a4975099b2d7689f7032c8df4ad4ef1a2b7f7fb2
Nakashita <account@nakashita.fr>
2021-10-19 14:26:57 +0200

    tmp

--- src/main/java/Colis.java
+++ src/main/java/Colis.java
@@ -1,5 +1,5 @@
 
-public class Colis {
+public class Colis{
 	public static int ID = 0;
 	private final int id = ID++;
 	private double poids;
@@ -13,4 +13,26 @@ public class Colis {
 		this.origine = src;
 		this.destination = dest;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public double getPoids() {
+		return poids;
+	}
+
+	public String getVal() {
+		return val;
+	}
+
+	public Emplacement getOrigine() {
+		return origine;
+	}
+
+	public Emplacement getDestination() {
+		return destination;
+	}
+	
+	
 }
